6b84080ab8bff2e374e174149687e17738696e14
wangbiao <wangbiao>
2017-03-31 11:13:56 +0800

    更改提交的脚本

--- publish.sh
+++ publish.sh
@@ -3,14 +3,17 @@
 echo "项目提交: $1"
 find ./ -name ".DS_Store" -exec rm {} \;
 git status
-git add .
 
 if [ "$1" = "" ] ;then    
 	echo "请填写提交详情    再执行脚本提交..."
-	exit
+	exit 1  
 elif ["$1" != "" ];then
 	echo "正在准备提交请等待。。。。"
+	echo  '提交记录日志为：    $1'
+	pause
 fi
+
+git add .
 git commit -m "$1"
 git push origin master
-
+echo '代码更新完成'
