8f2b2a23c7b71035cf720ff0841dbc09e2b6b876
wangbiao <wangbiao>
2017-04-06 16:10:43 +0800

    修复bug

--- ssmShiroDemo/src/test/java/com/serialize/serializeTest.java
+++ ssmShiroDemo/src/test/java/com/serialize/serializeTest.java
@@ -1,7 +1,10 @@
 package com.serialize;
 
-import org.junit.Test;
+import java.util.HashSet;
+import java.util.Set;
 
+import org.apache.shiro.session.Session;
+import org.junit.Test;
 
 import lombok.extern.slf4j.Slf4j;
 import redis.clients.jedis.Jedis;
@@ -53,6 +56,48 @@ public class serializeTest {
 			byte[] newValue=jedis.get(skey);
 			System.out.println(SerializeUtil.deserialize(newValue)+">>");
 	        jedis.close();
+	}
+	
+	/**
+	 * 获取session
+	 */
+	@SuppressWarnings("unchecked")
+	@Test
+	public void getSession()
+	{
+		 Set<Session> sessions = new HashSet<Session>();
+		Jedis jedis = new Jedis("localhost");
+		jedis.select(0);
+	//	jedis.set("35", "test");
+		//多少秒这个key失效
+		jedis.expire("35", 444);
+		System.out.println("剩余失效时间："+jedis.ttl("35"));;
+		//字符串，如果在键中设置了值则返回OK。如果没有  就会赋值上去、、、   设置超时时间
+		jedis.setex("35", 30, "5444454");
+		System.out.println("剩余失效时间："+jedis.ttl("35"));;
+		Set<byte[]> b=jedis.keys(("*3*").getBytes());
+		System.out.println(b.size()+">>");
+//		for (byte[] bs : b) 
+//		{  
+//         	 System.out.println(bs);
+//        }
+		jedis.select(1);
+		System.out.println("----------------------------------------------分割线-------------");
+		Set<byte[]> byteKeys = jedis.keys(("*sojson-shiro-demo-session:*").getBytes());	
+		System.out.println("-------------------------------         "+byteKeys.size());
+         if (byteKeys != null && byteKeys.size() > 0) {  
+             for (byte[] bs : byteKeys) {  
+             	Session obj = SerializeUtil.deserialize(jedis.get(bs)
+             			, Session.class);  
+             	System.out.println(obj+">>>>>>>>"+(obj instanceof Session));
+                  if(obj instanceof Session){
+                 	 sessions.add(obj);  
+                  }
+             }  
+         }  
+         System.out.println(sessions.size()+">?....");
+         
+		jedis.close();
 	}
 	
 }
