09bf4234993c3c4cf070b112537d12b745d4bd59
wangbiaoJava <229492022@qq.com>
2017-05-07 22:45:08 +0800

    能添加数据

--- ActivitiDemo/src/main/test/FirstActiviti.java
+++ ActivitiDemo/src/main/test/FirstActiviti.java
@@ -13,9 +13,6 @@ import java.util.List;
 public class FirstActiviti
 {
 
-    /**
-     * 部署流程定义
-     */
     ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
 
     /**部署流程定义*/
@@ -24,52 +21,55 @@ public class FirstActiviti
         Deployment deployment = processEngine.getRepositoryService()//与流程定义和部署对象相关的Service
                 .createDeployment()//创建一个部署对象
                 .name("helloworld入门程序")//添加部署的名称
+
                 .addClasspathResource("bmpb/helloworld.bpmn")//从classpath的资源中加载，一次只能加载一个文件
-                //	.addClasspathResource("diagrams/helloworld.png")//从classpath的资源中加载，一次只能加载一个文件
+                .addClasspathResource("bmpb/helloworld.png")//从classpath的资源中加载，一次只能加载一个文件
                 .deploy();//完成部署
         System.out.println("部署ID："+deployment.getId());//1
         System.out.println("部署名称："+deployment.getName());//helloworld入门程序
     }
-    /**
-     * 启动流程实例
-     */
-    @Test
-    public void startProcessInstance() {
-        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
-        String processDefinitionKey = "helloworld";
-        ProcessInstance pi = processEngine.getRuntimeService()
-                .startProcessInstanceByKey(processDefinitionKey);
 
-        System.out.println("流程实例ID：" + pi.getId());
-        System.out.println("流程定义ID：" + pi.getProcessDefinitionId());
+    /**启动流程实例*/
+    @Test
+    public void startProcessInstance(){
+        //流程定义的key
+        String processDefinitionKey = "helloworld";
+        ProcessInstance pi = processEngine.getRuntimeService()//与正在执行的流程实例和执行对象相关的Service
+                .startProcessInstanceByKey(processDefinitionKey);//使用流程定义的key启动流程实例，key对应helloworld.bpmn文件中id的属性值，使用key值启动，默认是按照最新版本的流程定义启动
+        System.out.println("流程实例ID:"+pi.getId());//流程实例ID    101
+        System.out.println("流程定义ID:"+pi.getProcessDefinitionId());//流程定义ID   helloworld:1:4
     }
 
-    /**
-     * 查询当前人的个人认为
-     */
+
+    /**查询当前人的个人任务*/
     @Test
-    public void findMyPersonTask() {
-        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+    public void findMyPersonalTask(){
         String assignee = "张三";
-        List<Task> list = processEngine.getTaskService().createTaskQuery()
-                .taskAssignee(assignee).list();
-        for (Task task : list) {
-            System.out.println(task.getId());
-            System.out.println(task.getName());
-            System.out.println(task.getCreateTime());
-            System.out.println(task.getAssignee());
+        List<Task> list = processEngine.getTaskService()//与正在执行的任务管理相关的Service
+                .createTaskQuery()//创建任务查询对象
+             //   .taskAssignee(assignee)//指定个人任务查询，指定办理人
+                .list();
+        if(list!=null && list.size()>0){
+            for(Task task:list){
+                System.out.println("任务ID:"+task.getId());
+                System.out.println("任务名称:"+task.getName());
+                System.out.println("任务的创建时间:"+task.getCreateTime());
+                System.out.println("任务的办理人:"+task.getAssignee());
+                System.out.println("流程实例ID："+task.getProcessInstanceId());
+                System.out.println("执行对象ID:"+task.getExecutionId());
+                System.out.println("流程定义ID:"+task.getProcessDefinitionId());
+                System.out.println("########################################################");
+            }
         }
     }
 
-    /**
-     * 完成我的任务
-     */
+    /**完成我的任务*/
     @Test
-    public void completeMyPersonTask() {
-        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
-        String taskId = "104";
-        processEngine.getTaskService().complete(taskId);
-
-        System.out.println("完成任务：" + taskId);
+    public void completeMyPersonalTask(){
+        //任务ID
+        String taskId = "20001";
+        processEngine.getTaskService()//与正在执行的任务管理相关的Service
+                .complete(taskId);
+        System.out.println("完成任务：任务ID："+taskId);
     }
 }
---
+++ ActivitiDemo/src/main/test/beginActiviti.java
@@ -0,0 +1,45 @@
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngineConfiguration;
+import org.junit.Test;
+
+/**
+ * Created by wangbiao on 2017/5/7.
+ *
+ * 初始创建工作需要的23张表
+ */
+public class beginActiviti {
+    /**
+     * 生成25张Activiti表
+     */
+    @Test
+    public void testCreateTable() {
+        // 引擎配置
+        ProcessEngineConfiguration pec=ProcessEngineConfiguration.createStandaloneProcessEngineConfiguration();
+        pec.setJdbcDriver("com.mysql.jdbc.Driver");
+        //processEngineConfiguration.setJdbcUrl("jdbc:mysql://localhost:3306/itcast0711activiti?useUnicode=true&characterEncoding=utf8");
+        pec.setJdbcUrl("jdbc:mysql://localhost:3306/activiti");
+        pec.setJdbcUsername("root");
+        pec.setJdbcPassword("123456");
+
+        /**
+         * DB_SCHEMA_UPDATE_FALSE 不能自动创建表，需要表存在
+         * create-drop 先删除表再创建表
+         * DB_SCHEMA_UPDATE_TRUE 如何表不存在，自动创建和更新表
+         */
+        pec.setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
+
+        // 获取流程引擎对象
+        ProcessEngine processEngine=pec.buildProcessEngine();
+    }
+
+    /**
+     * 使用xml配置 简化
+     */
+    @Test
+    public void testCreateTableWithXml(){
+        // 引擎配置
+        ProcessEngineConfiguration pec=ProcessEngineConfiguration.createProcessEngineConfigurationFromResource("activiti.cfg.xml");
+        // 获取流程引擎对象
+        ProcessEngine processEngine=pec.buildProcessEngine();
+    }
+}
